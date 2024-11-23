55186f9039be756c77206388f890ce4629ea0667
JD297 <jan.dommasch297@gmail.com>
2024-11-23 11:16:16 +0100

    Added file types and protection bits

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -3,6 +3,26 @@
 #ifndef JD297_LIBC_SYS_STAT_H
 #define JD297_LIBC_SYS_STAT_H
 
+/* File types. */
+#define S_IFMT		0170000	/* These bits determine file type. */
+
+#define S_IFDIR		0040000	/* Directory. */
+#define S_IFCHR		0020000	/* Character device. */
+#define S_IFBLK		0060000	/* Block device. */
+#define S_IFREG		0100000	/* Regular file. */
+#define S_IFIFO		0010000	/* FIFO.  */
+#define S_IFLNK		0120000	/* Symbolic link. */
+#define S_IFSOCK	0140000	/* Socket. */
+
+/* Protection bits. */
+
+#define S_ISUID		04000	/* Set user ID on execution. */
+#define S_ISGID		02000	/* Set group ID on execution. */
+#define S_ISVTX		01000	/* Save swapped text after use (sticky). */
+#define S_IREAD		0400	/* Read by owner. */
+#define S_IWRITE	0200	/* Write by owner. */
+#define S_IEXEC		0100	/* Execute by owner. */
+
 typedef struct stat {
 	dev_t     st_dev;
 	ino_t     st_ino;
