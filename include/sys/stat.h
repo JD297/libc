b1436d9282c59f4d334f38a451504023c9821861
JD297 <jan.dommasch297@gmail.com>
2024-11-23 11:18:01 +0100

    Fixed whitespaces in include/sys/stat.h

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -24,19 +24,19 @@
 #define S_IEXEC		0100	/* Execute by owner. */
 
 typedef struct stat {
-	dev_t     st_dev;
-	ino_t     st_ino;
-	mode_t    st_mode;
-	nlink_t   st_nlink;
-	uid_t     st_uid;
-	gid_t     st_gid;
-	dev_t     st_rdev;
-	off_t     st_size;
-	blksize_t st_blksize;
-	blkcnt_t  st_blocks;
-	time_t    st_atime;
-	time_t    st_mtime;
-	time_t    st_ctime;
+	dev_t		st_dev;
+	ino_t		st_ino;
+	mode_t		st_mode;
+	nlink_t		st_nlink;
+	uid_t		st_uid;
+	gid_t		st_gid;
+	dev_t		st_rdev;
+	off_t		st_size;
+	blksize_t	st_blksize;
+	blkcnt_t	st_blocks;
+	time_t		st_atime;
+	time_t		st_mtime;
+	time_t		st_ctime;
 } stat;
 
 extern int stat(const char *restrict pathname, struct stat *restrict statbuf);
