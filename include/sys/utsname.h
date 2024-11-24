89a9b303df1c50049c2078c4e51bdbd5d71ebb11
JD297 <jan.dommasch297@gmail.com>
2024-11-24 13:02:40 +0100

    Fixed utsname uses kernel headers

--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -1,15 +1,17 @@
+#include <linux/utsname.h>
+
 #ifndef JD297_LIBC_SYS_UTSNAME
 #define JD297_LIBC_SYS_UTSNAME
 
-#define _UTSNAME_LENGTH 65
+#define _UTSNAME_LENGTH __NEW_UTS_LEN
 
 typedef struct utsname {
-	char sysname[_UTSNAME_LENGTH];
-	char nodename[_UTSNAME_LENGTH];
-	char release[_UTSNAME_LENGTH];
-	char version[_UTSNAME_LENGTH];
-	char machine[_UTSNAME_LENGTH];
-	char domainname[_UTSNAME_LENGTH];
+	char sysname[_UTSNAME_LENGTH + 1];
+	char nodename[_UTSNAME_LENGTH + 1];
+	char release[_UTSNAME_LENGTH + 1];
+	char version[_UTSNAME_LENGTH + 1];
+	char machine[_UTSNAME_LENGTH + 1];
+	char domainname[_UTSNAME_LENGTH +1];
 };
 
 #endif
