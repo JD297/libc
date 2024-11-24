be195d971ec51f181c97de17c7128a216ffc2d5e
JD297 <jan.dommasch297@gmail.com>
2024-11-24 13:48:50 +0100

    Fixed sys/mman.h with kernel-headers

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -1,9 +1,11 @@
-#include <asm-generic/mman.h>
+#include <linux/mman.h>
 #include <stddef.h>
 #include <sys/types.h>
 
 #ifndef JD297_LIBC_SYS_MMAN_H
 #define JD297_LIBC_SYS_MMAN_H
+
+#define MAP_FAILED ((void *) -1)
 
 extern void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 
